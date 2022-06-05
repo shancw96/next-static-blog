2b232cb55b82be139ba91e22657188ca64aad887
shancw <shancw1996@gmail.com>
2022-06-05 16:12:54 +0800

    chore: update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 highlight='\033[0;31m'
 NC='\033[0m'
 echo "${highlight}---------------------------------------------${NC}"
-
+cd next-static-blog
 image_name=127.0.0.1:5000/staticblog:v1
 container_name=staticblog
-echo delete old container
+echo "${highlight}delete old container${NC}" 
 docker rm -f ${container_name}
-echo delete old image
+echo "${highlight}delete old image${NC}" 
 docker rmi $image_name
 
 docker rmi --force $image_name
-echo building docker image...
-docker build . -t $image_name --no-cache
-echo running docker image...
+echo "${highlight}building image...${NC}" 
+docker build /home/shancw/project/next-static-blog/. -t $image_name --no-cache
+echo "${highlight}running image...${NC}" 
 docker run -d -t -p 8088:3000 --name=$container_name --restart=always $image_name
 
 exit 0;
