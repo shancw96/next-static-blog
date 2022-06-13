53b1c8ffde06c407d67b36f21bb047a6e6c455c6
shanCW <shancw1996@gmail.com>
2022-06-13 21:07:59 +0800

    fix: sort by createTime

--- lib/api.ts
+++ lib/api.ts
@@ -48,6 +48,6 @@ export function getAllPosts(fields: (keyof PostType)[] = []) {
   const posts = slugs
     .map((slug) => getPostBySlug(slug, fields))
     // sort posts by date in descending order
-    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1))
+    .sort((post1, post2) => (new Date(post1.date) > new Date(post2.date) ? -1 : 1))
   return posts
 }
