ee9471a7de33e9a5420a6c64ed75bb765363c5ba
shanCW <shancw1996@gmail.com>
2022-06-30 13:35:27 +0800

    chore: add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'http://serial.limiaomiao.site:8088',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
+module.exports = config
